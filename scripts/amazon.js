bea1013b62b43b206fff2f684e57845082948b3a
Keen <can.wang.keen@gmail.com>
2025-12-18 02:45:44 +0100

    use product array in data folder to generate all products in the grid.

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,6 +1,10 @@
 // data structure of the products in grid.
+// data folder already provided the array, hence just use it.
+// remember js code run one by one in html file, so order matters.
+/*
 const products = [
     {
+        //1st
         image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
         name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
         rating: {
@@ -9,6 +13,7 @@ const products = [
         },
         priceCents: 1090
     }, {
+        //2nd
         image: 'images/products/intermediate-composite-basketball.jpg',
         name: 'Intermediate Size Basketball',
         rating: {
@@ -18,6 +23,7 @@ const products = [
         priceCents: 2095
 
     }, {
+        // 3rd
         image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
         name: 'Adults Plain Cotton T-Shirt - 2 Pack',
         rating: {
@@ -26,8 +32,38 @@ const products = [
         },
         priceCents: 799
 
-    }];
+    }, {
+        // 4th
+        image: 'images/products/black-2-slot-toaster.jpg',
+        name: '2 Slot Toaster - Black',
+        rating: {
+            stars: 4,
+            count: 2197
+        },
+        priceCents: 1899
 
+    }, {
+        // 5th
+        image: 'images/products/6-piece-white-dinner-plate-set.jpg',
+        name: '6 Piece White Dinner Plate Set',
+        rating: {
+            stars: 4,
+            count: 37
+        },
+        priceCents: 2067
+
+    }, {
+        // 6rd
+        image: 'images/products/6-piece-non-stick-baking-set.webp',
+        name: '6-Piece Nonstick, Carbon Steel Oven Bakeware Baking Set',
+        rating: {
+            stars: 4.5,
+            count: 175
+        },
+        priceCents: 3499
+
+    }];
+*/
 let productsHTML = '';
 // loop through the product array and create single product's html
 products.forEach((product) => {
