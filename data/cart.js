26ace5a0a5c74c06d3943fae95fce5ff83d9fe3a
Keen <can.wang.keen@gmail.com>
2025-12-19 02:14:58 +0100

    delete products from cart when clicking delete

--- data/cart.js
+++ data/cart.js
@@ -1,6 +1,6 @@
 // for clearer structure, seperate the cart object from the webside.
 // export can let the variable used by other files outside of cart.js
-export const cart = [{
+export let cart = [{
     productId : 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
     quantity : 2
 }, {
@@ -34,4 +34,15 @@ export function addToCart(productId) {
         // console.log(cart);
 
     }
+}
+
+export function removeFromCart(productId) {
+    const newCart = [];
+    cart.forEach(cartItem => {
+        if (cartItem.productId !== productId) {
+            newCart.push(cartItem);
+        }
+    });
+
+    cart = newCart;
 }
