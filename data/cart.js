5e06923c18abdfaaa8d1342dfbff36a5741cfb3b
Keen <can.wang.keen@gmail.com>
2025-12-19 01:49:22 +0100

    generate HTML with js in checkout.js onto checkout.html, for the order summary.

--- data/cart.js
+++ data/cart.js
@@ -1,6 +1,13 @@
 // for clearer structure, seperate the cart object from the webside.
 // export can let the variable used by other files outside of cart.js
-export const cart = [];
+export const cart = [{
+    productId : 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+    quantity : 2
+}, {
+    productId : '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+    quantity : 1
+} ];
+// we can just use the id to search other info of this product.
 
 // write functions for adding to cart:
 // also group related code together into its own file.
