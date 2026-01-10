75f648d5fbf5448db27494ce04aefadde75fb971
Keen <can.wang.keen@gmail.com>
2026-01-10 14:05:51 +0100

    debugging

--- scripts/checkout/paymentSummary.js
+++ scripts/checkout/paymentSummary.js
@@ -54,10 +54,15 @@ export function renderPaymentSummary() {
             <div class="payment-summary-money js-payment-summary-total">$${formatCurrency(totalCents)}</div>
           </div>
 
-          <button class="place-order-button button-primary">
+          <button class="place-order-button button-primary
+          js-place-order">
             Place your order
           </button>
     `;
 
   document.querySelector('.js-payment-summary').innerHTML = paymentSummaryHTML;
+
+  document.querySelector('.js-place-order').addEventListener('click', () =>{
+
+  });
 }
